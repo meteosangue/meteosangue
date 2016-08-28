8c5afe04245a90264de255f41a4f53f85a02855e
Andrea Stagi <stagi.andrea@gmail.com>
2016-08-28 15:46:26 +0200

    Fix locale crash

--- core/utils.py
+++ core/utils.py
@@ -11,7 +11,10 @@ Method to get date from CRS output
 """
 def crs_to_date(date):
     my_tz = pytz.timezone('Europe/Rome')
-    locale.setlocale(locale.LC_TIME, "it_IT.utf8")
+    try:
+        locale.setlocale(locale.LC_TIME, "it_IT.utf8")
+    except:
+        locale.setlocale(locale.LC_TIME, "it_IT")
     clean1 = date.replace('Aggiornato a\xa0', '').replace('\xa0alle\xa0', ' ').split(' ')
     clean2 = ' '.join(clean1[1:])
     return my_tz.localize(datetime.strptime(clean2, "%d %B %Y %H:%M"))
