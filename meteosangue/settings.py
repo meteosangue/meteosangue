30189d3b432ed2b3b4946c36358a9256d20b9635
Andrea Stagi <stagi.andrea@gmail.com>
2016-08-30 00:44:40 +0200

    Bump version number to 1.0.0

--- meteosangue/settings.py
+++ meteosangue/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os, sys
 
+VERSION = '1.0.0'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
