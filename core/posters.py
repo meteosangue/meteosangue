64ba8ae0663bd123c86f003c711a17e2f2a6e13f
Andrea Stagi <stagi.andrea@gmail.com>
2016-09-18 11:13:53 +0200

    Send photo before text

--- core/posters.py
+++ core/posters.py
@@ -28,8 +28,8 @@ Method to post with image on Telegram
 def telegram_status(status, image_path=None):
     try:
         bot = telepot.Bot(settings.TELEGRAM_TOKEN)
-        bot.sendMessage(settings.TELEGRAM_CHANNEL, status)
         if image_path:
             bot.sendPhoto(settings.TELEGRAM_CHANNEL, open(image_path, "rb"))
+        bot.sendMessage(settings.TELEGRAM_CHANNEL, status)
     except telepot.exception.TelepotException as ex:
         raise MeteoSangueException(ex)
