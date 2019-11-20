27e79ae53abfe189730d818657ba50cd44638934
Andrea Stagi <stagi.andrea@gmail.com>
2019-11-20 20:25:43 +0100

    Put rqworker in background

--- docker-startup.sh
+++ docker-startup.sh
@@ -8,6 +8,6 @@ done
 echo 'Update status'
 python manage.py shell -c 'from core.tasks import fetch_and_update; fetch_and_update()'
 echo 'Run cron in background'
-python manage.py rqworker
+python manage.py rqworker  &
 echo 'Run gunicorn'
 exec gunicorn meteosangue.wsgi
